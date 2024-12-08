35b9dbaa0856dc0104219e2b4c0c800aee76de55
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-08 17:28:30 +0900

    update

--- webapp/go/internal_handlers.go
+++ webapp/go/internal_handlers.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
@@ -19,23 +21,32 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	defer tx.Rollback()
 
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+	rides := make([]*Ride, 0)
+	if err := tx.SelectContext(ctx, &rides, `SELECT id FROM rides WHERE chair_id IS NULL`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
+			if err := tx.Commit(); err != nil {
+				writeError(w, http.StatusInternalServerError, err)
+				return
+			}
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-		writeError(w, http.StatusInternalServerError, err)
+		writeError(w, http.StatusInternalServerError, fmt.Errorf("failed to get rides: %w", err))
 		return
 	}
 
-	matched := &Chair{}
-	if err := tx.GetContext(
+	matched := make([]string, 0, len(rides))
+	if err := tx.SelectContext(
 		ctx,
-		matched,
-		`SELECT id FROM chairs WHERE is_active = TRUE AND available = TRUE LIMIT 1`,
+		&matched,
+		`SELECT id FROM chairs WHERE is_active = TRUE AND available = TRUE LIMIT ?`,
+		len(rides),
 	); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
+			if err := tx.Commit(); err != nil {
+				writeError(w, http.StatusInternalServerError, err)
+				return
+			}
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
@@ -43,46 +54,23 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// matched := &Chair{}
-	// empty := false
-	// for i := 0; i < 10; i++ {
-	// 	if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-	// 		if errors.Is(err, sql.ErrNoRows) {
-	// 			w.WriteHeader(http.StatusNoContent)
-	// 			return
-	// 		}
-	// 		writeError(w, http.StatusInternalServerError, err)
-	// 	}
+	assignedRides := make([]*Ride, 0, len(matched))
+	for i, m := range matched {
+		assignedRides = append(assignedRides, rides[i])
+		assignedRides[i].ChairID = sql.NullString{String: m, Valid: true}
+	}
 
-	// 	if err := db.GetContext(
-	// 		ctx,
-	// 		&empty,
-	// 		`SELECT COUNT(*) = 0 FROM (
-	// 				SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses
-	// 				WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?)
-	// 				GROUP BY ride_id
-	// 			)
-	// 			is_completed WHERE completed = FALSE`,
-	// 		matched.ID,
-	// 	); err != nil {
-	// 		writeError(w, http.StatusInternalServerError, err)
-	// 		return
-	// 	}
-	// 	if empty {
-	// 		break
-	// 	}
-	// }
-	// if !empty {
-	// 	w.WriteHeader(http.StatusNoContent)
-	// 	return
-	// }
-
-	if _, err := tx.ExecContext(ctx, "UPDATE chairs SET available = FALSE WHERE id = ?", matched.ID); err != nil {
+	query, args, err := sqlx.In("UPDATE chairs SET available = FALSE WHERE id IN (?)", matched)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, fmt.Errorf("failed to get available chair: %w", err))
+		return
+	}
+	if _, err := tx.ExecContext(ctx, query, args...); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	if _, err := tx.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	if _, err := tx.NamedExecContext(ctx, "UPDATE rides SET chair_id = :chair_id WHERE id = :id", assignedRides); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
