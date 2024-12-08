6135587a191ba2c10b0434043166e648002e517b
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-08 17:51:01 +0900

    fix table name:

--- webapp/sql/4-alter-table.sql
+++ webapp/sql/4-alter-table.sql
@@ -1,4 +1,4 @@
 ALTER TABLE chairs
 ADD COLUMN `available` BOOLEAN NOT NULL DEFAULT TRUE COMMENT '椅子が利用可能かどうか';
 
-ALTER TABLE charis ADD INDEX `is_active_available_idx` (`is_active`, `available`);
+ALTER TABLE chairs INDEX `is_active_available_idx` (`is_active`, `available`);
