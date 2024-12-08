389084a2aad11d24ceccc238c0003c16ac083c39
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-08 11:30:12 +0900

    add createdat idx on chair_loc

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -45,7 +45,8 @@ CREATE TABLE chair_locations (
   latitude INTEGER NOT NULL COMMENT '経度',
   longitude INTEGER NOT NULL COMMENT '緯度',
   created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '登録日時',
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  KEY `created_at_desc_idx` (`created_at` DESC)
 ) COMMENT = '椅子の現在位置情報テーブル';
 
 DROP TABLE IF EXISTS users;
