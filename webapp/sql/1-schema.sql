e0a099aa701e0a36db8db64ac443f787959e2b36
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-08 13:49:35 +0900

    add token index

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -34,7 +34,8 @@ CREATE TABLE chairs (
   created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '登録日時',
   updated_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新日時',
   PRIMARY KEY (id),
-  KEY `owner_id_idx` (`owner_id`)
+  KEY `owner_id_idx` (`owner_id`),
+  KEY `access_token_idx` (`access_token`)
 ) COMMENT = '椅子情報テーブル';
 
 DROP TABLE IF EXISTS chair_locations;
