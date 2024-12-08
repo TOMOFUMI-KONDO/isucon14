ace3d2cecb54a0f1037966608cc4911e7509acd9
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-08 12:53:39 +0900

    fix

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -169,7 +169,7 @@ func initializeChairDistances(ctx context.Context) error {
 		distances[i].ID = ulid.Make().String()
 	}
 
-	if _, err := db.NamedExecContext(ctx, `INSERT INTO chair_distances (id, chair_id, distance, created_at) VALUES (:id, :chair_id, :distance, :created_at`, distances); err != nil {
+	if _, err := db.NamedExecContext(ctx, `INSERT INTO chair_distances (id, chair_id, distance, created_at) VALUES (:id, :chair_id, :distance, :created_at)`, distances); err != nil {
 		return fmt.Errorf("failed to insert chair distances: %w", err)
 	}
 
