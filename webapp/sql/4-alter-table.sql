0a5f3623dbf2ba84317a4924f8e2774c34c7df9b
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-13 12:01:34 +0900

    alter after initialize

--- webapp/sql/4-alter-table.sql
+++ webapp/sql/4-alter-table.sql
@@ -2,3 +2,6 @@ ALTER TABLE chairs
 ADD COLUMN `available` BOOLEAN NOT NULL DEFAULT TRUE COMMENT '椅子が利用可能かどうか';
 
 ALTER TABLE chairs ADD INDEX `is_active_available_idx` (`is_active`, `available`);
+
+ALTER TABLE rides
+ADD COLUMN notified_completed TINYINT (1) NOT NULL DEFAULT 0 COMMENT '完了通知済みかどうか',
